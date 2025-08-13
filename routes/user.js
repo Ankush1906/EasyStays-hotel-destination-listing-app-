d687331eded0baebb733503d1673bfd9407eb72a
Ankush choudhary <ankushchoudhary6265@gmail.com>
2025-08-13 12:04:14 +0530

    Rename wrapAsync.js and fix case in imports

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router =  express.Router({});
 const User = require("../models/user.js");
-const wrapAsync = require("../utils/wrapasync");
+const wrapAsync = require("../utils/wrapAsync");
 const passport = require("passport");
 const { saveRedirectUrl } = require("../middleware.js");
 const  userContrrollers = require("../controllers/users.js");
